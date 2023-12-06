cd5fe81341feeeac5ba31efa05618f335bc6a677
qiurongcan <105793485+qiurongcan@users.noreply.github.com>
2023-12-06 15:54:16 +0800

    Add files via upload

--- Comments/comments2.py
+++ Comments/comments2.py
@@ -24,7 +24,7 @@ resp1.close()
 
 # ------------------定值--------------------- #
 ct="ea1db124af3c7062474693fa704f4ff8"
-
+"ea1db124af3c7062474693fa704f4ff8"
 header2={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
     "Cookie":"buvid3=87AB7053-858C-6093-B4CF-4A3CD150773376912infoc; b_nut=1695788176; i-wanna-go-back=-1; b_ut=7; _uuid=4A989FC6-FFDF-B15D-1EFA-77EF610BAEB9F75574infoc; buvid_fp=a1709f14cbd2b8a5c1fef5b37aab8ea2; buvid4=8743E9DB-5F18-800A-7E83-8359CA1BF4D677562-023092712-PdJr0jKE6N5pSQNdyTYMzr8F2IhY9DzV; home_feed_column=5; CURRENT_FNVAL=4048; rpdid=0z9Zw2XHhL|NXO5GbV6|gz|3w1QLlYS; DedeUserID=476956332; DedeUserID__ckMd5=8bb8ec02cf438f22; hit-dyn-v2=1; enable_web_push=DISABLE; header_theme_version=CLOSE; LIVE_BUVID=AUTO1616989118604773; bp_video_offset_476956332=866271891190448181; PVID=1; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDIwMTM1NzEsImlhdCI6MTcwMTc1NDMxMSwicGx0IjotMX0.aD__KJ-pV9WBQCt-wekR6eZA-4H57XqqcmRHuRysqms; bili_ticket_expires=1702013511; SESSDATA=97586540%2C1717306372%2C74114%2Ac1CjBsSFaCHhPrjxmOj2MCE6WCS9H5hO3mzl7edFjwXxbIQNJXUNDwv9SV6_GziyWckl4SVjI3UjlJbkhhYnpXNDVYajVvbjF2YklzNVJYdUVRMUFNbVNjZUdfMks1VUduSE0xb2hGRm9QYlAweGRJVERub3ZHQmNvcW14ZEhaMEpqVU5GdzlEWTlRIIEC; bili_jct=50ae985be86cba663bdf669a8e12452c; sid=4xea3k1c; fingerprint=9d56312efc56e939901cacbfe8ab854a; b_lsid=894108A87_18C3D498EF3; bsource=search_bing; browser_resolution=1872-966",
@@ -63,7 +63,7 @@ def req_other():
 
     wts2=int(time.time())
     # --获取pagination_str2
-    pagination_str2='{"offset":"{\"type\":1,\"direction\":1,\"session_id\":\"'+session_id+'\",\"data\":{}}"}'
+    pagination_str2=r'{"offset":"{\"type\":1,\"direction\":1,\"session_id\":\"'+str(session_id)+r'\",\"data\":{}}"}'
 
     # 进行url编码
     pagination_str2=urllib.parse.quote(pagination_str2)
@@ -74,14 +74,24 @@ def req_other():
     w_rid2=md5((Ut2+ct).encode()).hexdigest()
 
     base_url2=f'https://api.bilibili.com/x/v2/reply/wbi/main?oid={oid}&type=1&mode=3&pagination_str={pagination_str2}&plat=1&web_location=1315875&w_rid={w_rid2}&wts={wts2}'
-        
+    # print(base_url2)
     resp=requests.get(url=base_url2,headers=header2)
-    print(resp.text)
-    # json_data2=json.loads(resp.text)
-    # reply1=json_data2['data']['replies'][0]['content']['message']
-    # print(reply1)
+    # print(resp.text)
+    json_data2=json.loads(resp.text)
+    reply1=json_data2['data']['replies'][2]['content']['message']
+    print(reply1)
 
+print("-------")
 time.sleep(1)
 
 req_other()
+print("-------")
+time.sleep(1)
+
+req_other()
+print("-------")
+time.sleep(1)
+
+req_other()
+
 
